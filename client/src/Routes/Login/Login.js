f22cbf538e46b1a2ec2fa1768b8d6fa58e4e3ef4
장재영 <jangjaeyeong@MacBook-Pro-2.local>
2020-08-29 14:36:21 +0900

    2차수정

--- client/src/Routes/Login/Login.js
+++ client/src/Routes/Login/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { loginUser } from "../../_actions/user_action";
-import { withRouter } from "react-router-dom";
-import SocialLogin from "./SocialLogin";
-import styled from "styled-components";
-import { TextField } from "@material-ui/core";
+import React, { useState } from "react"
+import { useDispatch } from "react-redux"
+import { loginUser } from "../../_actions/user_action"
+import { withRouter } from "react-router-dom"
+import SocialLogin from "./SocialLogin"
+import styled from "styled-components"
+import { TextField } from "@material-ui/core"
 const Container = styled.div`
-display: flex;
+  display: flex;
   justify-content: center;
   flex-direction: column;
   text-align: left;
@@ -17,89 +17,92 @@ display: flex;
   min-height: 100vh;
   font-weight: 400;
   font-family: "Noto Sans KR", sans-serif;
-`;
+`
 const Wrapper = styled.div`
-width: 450px;
+  width: 450px;
   margin: auto;
   background: rgba(0, 0, 0, 0.75);
   box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
   padding: 40px 55px 45px 55px;
   border-radius: 15px;
   transition: all 0.3s;
-`;
+`
 const TextWrapper = styled.div`
-margin-bottom:10px;
-`;
+  margin-bottom: 10px;
+`
 const MyTextField = styled(TextField)({
-backgroundColor:"white",
-width:"100%",
-borderRadius:"5px",
-});
+  backgroundColor: "white",
+  width: "100%",
+  borderRadius: "5px",
+})
 function Login(props) {
-  const dispatch = useDispatch();
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
+  const dispatch = useDispatch()
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
   const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.value);
-  };
+    setEmail(event.currentTarget.value)
+  }
   const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  };
+    setPassword(event.currentTarget.value)
+  }
   const onSubmitHandler = (event) => {
-    event.preventDefault(); //페이지 refresh 방지
+    event.preventDefault() //페이지 refresh 방지
     let body = {
       email: Email,
       password: Password,
-    };
+    }
     //redux action => loginUser는 action이름
     dispatch(loginUser(body)).then((response) => {
       if (response.payload.loginSuccess) {
-        window.localStorage.setItem("userId", response.payload.userId); //
-        props.history.push("/");
+        window.localStorage.setItem("userId", response.payload.userId) //
+        props.history.push("/")
       } else {
-        alert("로그인 실패");
+        alert("로그인 실패")
       }
-    });
-  };
+    })
+  }
   return (
     <Container>
-     
       <Wrapper>
-        <div style={{fontSize:"25px" , marginBottom:"20px"}}>로그인</div>
+        <div style={{ fontSize: "25px", marginBottom: "20px" }}>로그인</div>
         <form onSubmit={onSubmitHandler}>
           <TextWrapper>
             <MyTextField
               // placeholder="이메일"
               value={Email}
               onChange={onEmailHandler}
-          type="email"
-          label="이메일 주소 또는 전화번호"
-          variant="filled"
-          size="small"
-          />
+              type="email"
+              label="이메일 주소 또는 전화번호"
+              variant="filled"
+              size="small"
+            />
           </TextWrapper>
           <TextWrapper>
-          <MyTextField
-          size="small"
+            <MyTextField
+              size="small"
               onChange={onPasswordHandler}
               value={Password}
               label="비밀번호"
               type="password"
               autoComplete="current-password"
               variant="filled"
-              />
+            />
           </TextWrapper>
-          <div style={{ textAlign: "center"}}>
-            <button type="submit" className="btn btn-primary btn-block" style={{backgroundColor:"mediumslateblue", borderColor:"mediumslateblue"}} >
+          <div style={{ textAlign: "center" }}>
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              style={{ backgroundColor: "mediumslateblue", borderColor: "mediumslateblue" }}
+            >
               로그인
-          </button>
+            </button>
           </div>
           <SocialLogin />
           <br />
-          <a href="/sign-up">회원이 아니신가요?</a>
+          <a href="/sign-up">Bongflix 회원이 아닌가요? 지금 가입하세요.</a>
         </form>
-          </Wrapper>
-      </Container>
+      </Wrapper>
+    </Container>
     // <div className="auth-wrapper">
     //   <div className="auth-inner">
     //     <form onSubmit={onSubmitHandler}>
@@ -119,8 +122,8 @@ function Login(props) {
     //           placeholder="비밀번호"
     //           value={Password}
     //           onChange={onPasswordHandler}
-    //         />            
-    //       </div>        
+    //         />
+    //       </div>
     //       <button type="submit" className="btn btn-primary btn-block">
     //         로그인
     //       </button>
@@ -130,6 +133,6 @@ function Login(props) {
     //     </form>
     //   </div>
     // </div>
-  );
+  )
 }
-export default withRouter(Login);
+export default withRouter(Login)
